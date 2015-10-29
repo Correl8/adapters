2a18cf472490469e89d4f6815d67329a4f176db2
Samuel Rinnetmäki <samuel@iki.fi>
2015-10-29 14:59:22 +0200

    using correl8 package

--- expense/app.js
+++ expense/app.js
@@ -1,7 +1,20 @@
 var request = require("request");
-var nopt = require('nopt'),
- noptUsage = require("nopt-usage"),
- knownOpts = {
+var correl8 = require("correl8");
+var nopt = require('nopt');
+var noptUsage = require("nopt-usage");
+
+var c8 = correl8('expense');
+var fields = {
+  date: 'date',
+  c: 'integer',
+  category: 'string',
+  t: 'float',
+  type: 'string',
+  cost: 'float'
+};
+var apiUrl;
+
+var knownOpts = {
     'url': [String, null],
     'help': Boolean,
     'init': Boolean,
@@ -14,7 +27,9 @@ var nopt = require('nopt'),
     'i': ['--init'],
     'c': ['--clear'],
     'u': ['--url'],
+    'from': ['--start'],
     's': ['--start'],
+    'to': ['--end'],
     'e': ['--end']
   },
   description = {
@@ -26,96 +41,61 @@ var nopt = require('nopt'),
     'end': ' End date as YYYY-MM-DD'
   },
   options = nopt(knownOpts, shortHands, process.argv, 2);
+var firstDate = options['start'] || null;
+var lastDate = options['end'] || null;
 
 // console.log(options);
 if (options['help']) {
   console.log('Usage: ');
   console.log(noptUsage(knownOpts, shortHands, description));
-  process.exit();
 }
-
-var elasticsearch = require('elasticsearch');
-var client = new elasticsearch.Client({
-  host: 'localhost:9200',
-  log: 'warning'
-});
-
-var INDEX_BASE = 'correl8';
-var sensor = 'expense';
-var CONFIG_BASE = 'config-adapter';
-var CONFIG_INDEX = 'config';
-
-var firstDate = options['start'] || null;
-var lastDate = options['end'] || null;
-var apiUrl;
-var configIndex = {index: CONFIG_INDEX, type: CONFIG_BASE};
-
-if (options['url']) {
-  var params = configIndex;
-  params.id = CONFIG_BASE + '-' + sensor;
-  params.body = {id: params.id, url: options['url']};
-  client.index(params).then(function (response) {
-    console.log('Configuration saved.');
-  }).catch(function(error) {
-    console.warn(error);
+else if (options['url']) {
+  c8.config({url: options['url']}).then(function(){
+    console.log('Configuration stored.');
+    c8.release();
   });
-  process.exit();
 }
 else if (options['clear']) {
-}
-else {
-  client.indices.exists({index: CONFIG_INDEX}, function(error, response) {
-    if (!response) {
-        console.log('Usage: ');
-        console.log(noptUsage(knownOpts, shortHands, description));
-        // console.log('Configure by ' + process.argv[0] + ' ' + process.argv[1] + ' <expense_url>');
-    }
-    else {
-      getConfig(importData);
-    }
+  c8.clear().then(function(res) {
+    console.log('Index cleared.');
+    c8.release();
+  }).catch(function(error) {
+    console.trace(error);
+    c8.release();
   });
 }
-
-function getConfig(next) {
-  var params = configIndex;
-  params.q = 'id:' + CONFIG_BASE + '-' + sensor,
-  params.body = {
-    fields: ['url'],
-    size: 1
-  }
-  client.search(params, function (error, response) {
-    if (error) {
-      console.warn("Config search got error: " + JSON.stringify(error));
-      return;
-    }
-    if (response && response.hits && response.hits.hits[0]) {
-      apiUrl = response.hits.hits[0].fields.url;
-      next();
+else if (options['init']) {
+  c8.init(fields).then(function(res) {
+    console.log('Index initialized.');
+    c8.release();
+  }).catch(function(error) {
+    console.trace(error);
+    c8.release();
+  });
+}
+else {
+  c8.config().then(function(res) {
+    if (res.hits && res.hits.hits && res.hits.hits[0]) {
+      // console.log(res.hits.hits[0]);
+      apiUrl = res.hits.hits[0]._source['url'];
+      // console.log('Url set to ' + apiUrl);
+      importData();
     }
     else {
-      console.log('Usage: ');
+      console.log('Configure first using --url. Usage: ');
       console.log(noptUsage(knownOpts, shortHands, description));
-      // console.log('Configure by ' + process.argv[0] + ' ' + process.argv[1] + ' <expense_url>');
+      c8.release();
     }
   });
 }
 
 function importData(next) {
   // console.log('Getting first date...');
-  var query = {
-    index: INDEX_BASE + '-' + sensor,
-    type: sensor,
-    body: {
-      fields: ['timestamp'],
-      size: 1,
-      sort: [{'timestamp': 'desc'}],
-    }
-  };
-  client.search(query, function (error, response) {
-    if (error) {
-      console.warn("search got error: " + JSON.stringify(error));
-      return;
-    }
+  c8.search({
+    fields: ['timestamp'],
+    size: 1,
+    sort: [{'timestamp': 'desc'}],
+  }).then(function(response) {
     if (firstDate) {
       console.log("Setting first time to " + firstDate);
     }
@@ -124,12 +104,12 @@ function importData(next) {
       firstDate = new Date(response.hits.hits[0].fields.timestamp);
     }
     else {
-      console.warn("No previously indexed data, setting first time to 1!");
+      console.warn("No previously indexed data, setting first time to 0!");
       firstDate = new Date(0);
     }
-    var url = apiUrl + '&from=' + firstDate.getDay() + '.' + (firstDate.getMonth() + 1) + '.' + firstDate.getFullYear();
+    var url = apiUrl + '&from=' + firstDate.getDate() + '.' + (firstDate.getMonth() + 1) + '.' + firstDate.getFullYear();
     if (lastDate) {
-      url += '&to=' + lastDate.getDay() + '.' + (lastDate.getMonth() + 1) + '.' + lastDate.getFullYear();
+      url += '&to=' + lastDate.getDate() + '.' + (lastDate.getMonth() + 1) + '.' + lastDate.getFullYear();
     }
     var cookieJar = request.jar();
     // console.log(url);
@@ -145,7 +125,7 @@ function importData(next) {
           var dayData = data[i];
           for (var j=0; j<dayData.length; j++) {
             var id = dayData[j].date + '-' + dayData[j].t;
-            bulk.push({index: {_index: INDEX_BASE + '-' + sensor, _type: sensor, _id: id}});
+            bulk.push({index: {_index: c8.index(), _type: c8.type(), _id: id}});
             dayData[j].id = id;
             dayData[j].timestamp = dayData[j].date;
             bulk.push(dayData[j]);
@@ -153,22 +133,14 @@ function importData(next) {
           }
         }
         // console.log(bulk);
-        client.bulk(
-          {
-            index: INDEX_BASE + '-' + sensor,
-            type: sensor,
-            body: bulk
-          },
-          function (error, response) {
-            if (error) {
-              console.warn('ES Error: ' + error);
-            }
-          }
-        );
+        c8.bulk(bulk).then(function(result) {
+          console.log('Indexed ' + result.items.length + ' documents in ' + result.took + ' ms.');
+          c8.release();
+        }).catch(function(error) {
+          console.trace(error);
+          c8.release();
+        });
       }
     });
   });
-  if (next) {
-    next();
-  }
 }
