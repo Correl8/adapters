2a8a9eaa244e2c32f933f1757dd8b5bf19140009
Samuel Rinnetmäki <samuel@iki.fi>
2016-12-23 00:45:38 +0200

    using MovesApi package instead of Moves

--- moves/index.js
+++ moves/index.js
@@ -1,4 +1,4 @@
-var Moves = require('moves');
+var movesApi = require('moves-api').MovesApi;
 var dates = require('moves-date');
 var express = require('express');
 var moves;
@@ -6,18 +6,15 @@ var moves;
 var defaultPort = 3321;
 var defaultUrl = 'http://localhost:' + defaultPort + '/';
 // var MAX_DAYS = 50;
-var MAX_DAYS = 7;
+var MAX_DAYS = 6;
 var MS_IN_DAY = 24 * 60 * 60 * 1000;
 
 var adapter = {};
 
 adapter.sensorName = 'moves';
 
-var summaryIndex = 'moves-summary';
 var summaryType = 'moves-summary';
-var moveIndex = 'moves-move';
 var moveType = 'moves-move';
-var placeIndex = 'moves-place';
 var placeType = 'moves-place';
 
 adapter.types = [
@@ -106,13 +103,13 @@ adapter.types = [
 
 adapter.promptProps = {
   properties: {
-    client_id: {
+    clientId: {
       description: 'Enter your Moves client ID'.magenta
     },
-    client_secret: {
+    clientSecret: {
       description: 'Enter your Moves client secret'.magenta
     },
-    redirect_uri: {
+    redirectUri: {
       description: 'Enter your redirect URL'.magenta,
       default: defaultUrl
     },
@@ -127,38 +124,29 @@ adapter.storeConfig = function(c8, result) {
     var port = server.address().port;
     // console.log('Temporary Moves auth server listening at http://%s:%s', host, port);
   });
-  moves = new Moves(result);
-  var auth_url = moves.authorize({
-    scope: ['activity', 'location']
-  });
-  console.log('Authorize the app by opening the followin url in your browser:\n ' + auth_url);
+
+  moves = new movesApi(result);
+  var authUrl = moves.generateAuthUrl();
+  console.log('Authorize the app by opening the followin url in your browser:\n ' + authUrl);
   app.all('/', function (req, res) {
     if (req.query && req.query.code) {
-      // moves = new Moves(result);
-      moves.token(req.query.code, function(error, response, body) {
+      moves.getAccessToken(req.query.code, function(error, authData) {
         if (error) {
           console.trace(error);
           res.json(error);
           return;
         }
-        var rb = JSON.parse(response.body);
-        if (rb) {
-          Object.assign(conf, rb);
-          server.close();
-          return c8.config(conf).then(function(){
-            res.send('Access token saved.');
-            console.log('Configuration stored.');
-            c8.release();
-            process.exit();
-          }).catch(function(error) {
-            console.trace(error);
-          });
-        }
-        else {
-          res.send('Authorization failed');
-          console.error('Authorization failed');
-          console.trace(res);
-        }
+        conf.accessToken = authData.access_token;
+        conf.refreshToken = authData.refresh_token;
+        server.close();
+        return c8.config(conf).then(function(){
+          res.send('Access token saved.');
+          console.log('Configuration stored.');
+          c8.release();
+          process.exit();
+        }).catch(function(error) {
+          console.trace(error);
+        });
       });
     }
     else {
@@ -168,13 +156,13 @@ adapter.storeConfig = function(c8, result) {
 }
 
 adapter.importData = function(c8, conf, opts) {
-  moves = new Moves(conf);
+  moves = new movesApi(conf);
   return c8.type(summaryType).search({
     _source: ['timestamp'],
     size: 1,
     sort: [{'timestamp': 'desc'}],
   }).then(function(response) {
-    console.log('Getting first date...');
+    // console.log('Getting first date...');
     return c8.search({
       _source: ['timestamp'],
       size: 1,
@@ -187,39 +175,24 @@ adapter.importData = function(c8, conf, opts) {
       firstDate.setTime(lastDate.getTime() - (MAX_DAYS * MS_IN_DAY));
       if (opts.firstDate) {
         firstDate = new Date(opts.firstDate);
-        console.log('Setting first time to ' + firstDate);
+        // console.log('Setting first time to argument ' + firstDate);
       }
       else if (resp && resp.timestamp) {
         var d = new Date(resp.timestamp);
         // refetch yesterday for updated summary
-        firstDate.setTime(d.getTime() - MS_IN_DAY);
-        console.log('Setting first time to ' + firstDate);
+        firstDate.setTime(d.getTime());
+        // console.log('Setting first time to latest stored date ' + firstDate);
       }
       else {
         goneAsync = true;
-        return moves.get('/user/profile', conf.access_token, function(error, response, body) {
+        return moves.getProfile(function(error, user) {
           if (error) {
             console.trace(error);
             return;
           }
-          else if (response.body === 'expired_access_token') {
-            console.warn('access token expired');
-            refreshToken(c8, conf);
-          }
-          else {
-            var rb = JSON.parse(response.body);
-            if (!rb || rb.error) {
-              console.trace(rb.error);
-              refreshToken(c8, conf);
-            }
-            else {
-              var user = JSON.parse(body);
-              // console.log(user);
-              console.log('Setting first time to Moves date ' + user.profile.firstDate);
-              firstDate = dates.parseISODate(user.profile.firstDate);
-              return importData(c8, conf, firstDate, lastDate);
-            }
-          }
+          // console.log('Setting first time to Moves profile date ' + user.profile.firstDate);
+          firstDate = dates.parseISODate(user.profile.firstDate);
+          return importData(c8, conf, firstDate, lastDate);
         });
       }
       if (!goneAsync) {
@@ -238,94 +211,51 @@ function importData(c8, conf, firstDate, lastDate) {
   }
   if (lastDate.getTime() > (firstDate.getTime() + MAX_DAYS * MS_IN_DAY)) {
     lastDate.setTime(firstDate.getTime() + MAX_DAYS * MS_IN_DAY);
-    console.warn('Setting last date to ' + lastDate);
+    // console.warn('Time range of ' + MAX_DAYS + ' days exceeded. Setting last date to ' + lastDate);
   }
   var startTime = firstDate;
   // startTime.setDate(startTime.getDate() + 1);
   var fromDate = dates.day(startTime);
-  var end = dates.day(lastDate);
-  if (fromDate === dates.day(new Date())) {
+  var toDate = dates.day(lastDate);
+  // var end = dates.day(lastDate);
+  if (fromDate == dates.day(new Date())) {
     console.log('Today\'s data already exists! Try again tomorrow...');
   }
-  var dayCount = 0;
-  var doneCount = 0;
-  while (fromDate < end) {
-    toDate = fromDate;
-    moves.get('/user/storyline/daily?from=' + fromDate + '&to=' + toDate + '&trackPoints=true', conf.access_token, function(error, response, body) {
-      if (error) {
-        console.warn(error);
-        return;
-      }
-      else if (response.body === 'expired_access_token') {
-        console.warn('access token expired');
-        return refreshToken(c8, conf);
-      }
-      else if (!response.body) {
-        console.warn('No response body in history!');
-        return;
-      }
-      else if (response.body.substr(0, 1) != '[') {
-        console.warn('Invalid response body in history: ' + response.body)
-        return;
-      }
-      else {
-        var rb = JSON.parse(response.body);
-          if (!rb || rb.error) {
-            if (rb.error) {
-              console.warn(rb.error);
-            }
-          return refreshToken(c8, conf);
-        }
-        else {
-          // console.log(body);
-          var documents = JSON.parse(body);
-          var bulk = [];
-          for (var i=0; i<documents.length; i++) {
-            var document = documents[i];
-            console.log(document.date);
-            var prepared = splitToBulk(c8, prepareForElastic(document));
-            bulk = bulk.concat(prepared);
-          }
-          // console.log(JSON.stringify(bulk));
-          return c8.bulk(bulk).then(function(result) {
-            console.log('Indexed ' + result.items.length + ' documents in ' + result.took + ' ms.');
-          }).catch(function(error) {
-            console.trace(error);
-          });
-        }
-      }
-      if (parseInt(response.headers['x-ratelimit-minuteremaining']) < 1) {
-        console.warn('Minute limit exceeded!')
-        dayCount = MAX_DAYS;
-      }
-    });
-    if (dayCount++ >= MAX_DAYS) {
+  var opts = {from: fromDate, to: toDate, trackPoints: true};
+  // console.log(opts);
+  moves.getStoryline(opts, function(error, documents) {
+    if (error) {
+      console.warn(error);
       return;
     }
-    startTime.setDate(startTime.getDate() + 1);
-    fromDate = dates.day(startTime);
-  }
+    var bulk = [];
+    for (var i=0; i<documents.length; i++) {
+      var document = documents[i];
+      var prepared = splitToBulk(c8, prepareForElastic(document));
+      console.log(document.date + ': ' + prepared.length/2 + ' documents');
+      bulk = bulk.concat(prepared);
+    }
+    // for (var i=0; i<bulk.length; i++) {
+    //   console.log(JSON.stringify(bulk[i]));
+    // }
+    return c8.bulk(bulk).then(function(result) {
+      console.log('Indexed ' + result.items.length + ' documents in ' + result.took + ' ms.');
+    }).catch(function(error) {
+      console.trace(error);
+    });
+  });
 }
 
 function refreshToken(c8, conf) {
-  moves.refresh_token(conf.refresh_token, function(error, response, body) {
+  moves.refreshToken(conf.refreshToken, function(error, authData) {
     if (error) {
       console.warn('Refresh got error: ' + error);
       return;
     }
-    var rb = JSON.parse(response.body);
-    console.log(rb);
-    if (!rb || rb.error) {
-      if (rb.error === 'invalid_grant') {
-        console.trace(rb.error);
-      }
-      console.trace(rb.error);
-    }
-    else {
-      c8.conf(Object.assign(conf, rb)).then(function() {
-        console.log('Token refreshed. Try again!')
-      });
-    }
+    conf.accessToken = authData.access_token;
+    c8.conf(conf).then(function() {
+      console.log('Token refreshed. Try again!')
+    });
   });
 }
 
@@ -358,14 +288,27 @@ function prepareForElastic(document) {
 function splitToBulk(c8, document) {
   var d = document.date;
   var bulk = [];
-  // console.log(JSON.stringify(document));
-  bulk.push({index: {_index: c8.type(summaryType)._index, _type: c8._type, _id: d}});
-  bulk.push({id: d, timestamp: d, summary: document.summary, caloriesIdle: document.caloriesIdle, lastUpdate: document.lastUpdate});
+  if (document.summary && document.summary.length) {
+    for (var i=0; i<document.summary.length; i++) {
+/*
+      var doc = document.summary[i];
+      doc.timestamp = d;
+      doc.caloriesIdle = document.caloriesIdle,
+      doc.lastUpdate = document.lastUpdate
+      bulk.push({index: {_index: c8.type(summaryType)._index, _type: c8._type, _id: d}});
+      bulk.push(doc);
+*/
+      var id = d.getTime() + '-' + i;
+      bulk.push({index: {_index: c8.type(summaryType)._index, _type: c8._type, _id: id}});
+	bulk.push({timestamp: d, summary: document.summary[i], caloriesIdle: document.caloriesIdle, lastUpdate: dates.parseISODate(document.lastUpdate)});
+    }
+  }
   if (document.segments && document.segments.length) {
     for (var i=0; i<document.segments.length; i++) {
       var seg = document.segments[i];
       seg.timestamp = seg.startTime;
-      if (seg.type === 'place') {
+      seg.lastUpdate = dates.parseISODate(seg.lastUpdate)
+      if (seg.type == 'place') {
         // seg.position = seg.place.location.position;
         seg.duration = dates.parseISODate(seg.endTime) - dates.parseISODate(seg.startTime);
         seg.id = seg.timestamp + '-' + seg.place.id;
@@ -375,6 +318,7 @@ function splitToBulk(c8, document) {
         c8.type(moveType);
         seg.id = seg.timestamp + '-' + seg.activity;
       }
+      // console.log(JSON.stringify(seg));
       bulk.push({index: {_index: c8._index, _type: c8._type, _id: seg.id}});
       bulk.push(seg);
     }
