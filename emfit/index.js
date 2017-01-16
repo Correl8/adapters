0df6003617204f52d478649075bd6fe21a09f125
Samuel Rinnetmäki <samuel@iki.fi>
2017-01-16 12:21:33 +0200

    ignore minitrends and deep data

--- emfit/index.js
+++ emfit/index.js
@@ -7,15 +7,33 @@ var adapter = {};
 
 adapter.sensorName = 'emfit-qs';
 
-var precenseIndex = 'emfit-qs-presence';
+var presenceIndex = 'emfit-qs-presence';
 var trendIndex = 'emfit-qs-trend';
 
 adapter.types = [
   {
-    name: precenseIndex,
+    name: presenceIndex,
     fields: {
-      "timestamp": "date",
-    }
+    timestamp: "date",
+    hrv_rmssd_datapoints: {
+    },
+    bed_exit_periods: {
+      exit_start: "date",
+      exit_end: "date",
+      exit_duration: "integer"
+    },
+    tossnturn_datapoints: "date"
+    },
+    note: "text",
+/*
+    ignored for now...
+    measured_datapoints: {
+    },
+    nodata_periods: {
+    },
+    sleep_epoch_datapoints: {
+    },
+*/
   },
   {
     name: trendIndex,
@@ -46,7 +64,7 @@ adapter.storeConfig = function(c8, result) {
 };
 
 adapter.importData = function(c8, conf, opts) {
-  c8.type(precenseIndex).search({
+  c8.type(presenceIndex).search({
     _source: ['timestamp'],
     size: 1,
     sort: [{'timestamp': 'desc'}],
@@ -86,14 +104,97 @@ function importData(c8, conf, firstDate, lastDate) {
   qs.login(conf.username, conf.password).then(function(data) {
     var deviceId = data.device_settings[0].device_id
     qs.latest(deviceId).then(function(latest) {
+      if (latest.error) {
+        console.error('Could not index latest presence data: ' + latest.error);
+        return;
+      }
+      if (latest.navigation_data && latest.navigation_data.length) {
+        for (var i=0; i<latest.navigation_data.length; i++) {
+          var periodId = latest.navigation_data[i].id;
+          qs.presence(periodId, deviceId).then(function(presence) {
+            presence.time_start = new Date(presence.time_start * 1000);
+            presence.time_end = new Date(presence.time_end * 1000);
+            presence.timestamp = presence.time_end;
+            // TODO: bulk index all the data to separate types?
+            delete(presence.measured_datapoints);
+            delete(presence.nodata_periods);
+            delete(presence.sleep_epoch_datapoints);
+            delete(presence.hrv_rmssd_datapoints);
+            delete(presence.hrv_rmssd_hist_data);
+            delete(presence.navigation_data);
+            delete(presence.minitrend_datestamps);
+            delete(presence.minitrend_sleep_score);
+            delete(presence.minitrend_sleep_efficiency);
+            delete(presence.minitrend_sleep_duration);
+            delete(presence.minitrend_time_in_bed_duration);
+            delete(presence.minitrend_sleep_class_in_rem_duration);
+            delete(presence.minitrend_sleep_class_in_light_duration);
+            delete(presence.minitrend_sleep_class_in_deep_duration);
+            delete(presence.minitrend_sleep_class_in_rem_percent);
+            delete(presence.minitrend_sleep_class_in_light_percent);
+            delete(presence.minitrend_sleep_class_in_deep_percent);
+            delete(presence.minitrend_tossnturn_count);
+            delete(presence.minitrend_measured_hr_avg);
+            delete(presence.minitrend_measured_hr_max);
+            delete(presence.minitrend_measured_hr_min);
+            delete(presence.minitrend_measured_rr_avg);
+            delete(presence.minitrend_measured_rr_min);
+            delete(presence.minitrend_measured_rr_max);
+            delete(presence.minitrend_measured_activity_avg);
+            delete(presence.minitrend_hrv_rmssd_evening);
+            delete(presence.minitrend_hrv_rmssd_morning);
+            delete(presence.minitrend_hrv_lf);
+            delete(presence.minitrend_hrv_recovery_total);
+            delete(presence.minitrend_hrv_recovery_integrated);
+            delete(presence.minitrend_hrv_recovery_ratio);
+            delete(presence.minitrend_hrv_recovery_rate);
+            
+            if (presence.bed_exit_periods && presence.bed_exit_periods.length) {
+              for (var i=0; i<presence.bed_exit_periods.length; i++) {
+                presence.bed_exit_periods[i] = {
+                  exit_start: new Date(presence.bed_exit_periods[i][1] * 1000),
+                  exit_end: new Date(presence.bed_exit_periods[i][0] * 1000),
+                  exit_duration: presence.bed_exit_periods[i][0] - presence.bed_exit_periods[i][1]
+                }
+              }
+            }
+            if (presence.tossnturn_datapoints && presence.tossnturn_datapoints.length) {
+              for (var i=0; i<presence.tossnturn_datapoints.length; i++) {
+                presence.tossnturn_datapoints[i] = new Date(presence.tossnturn_datapoints[i] * 1000);
+              }
+            }
+            // console.log(presence);
+            return c8.type(presenceIndex).insert(presence).then(function(result) {
+              console.log(presence.timestamp);
+            }).catch(function(error) {
+              console.trace(error);
+            });
+          });
+        }
+      }
+/*
       latest.timestamp = new Date(latest.time_end * 1000);
-      return c8.type(precenseIndex).index(latest).then(function(result) {
-        console.log('Indexed ' + result.items.length + ' presence documents in ' + result.took + ' ms.');
+      if (latest.bed_exit_periods && latest.bed_exit_periods.length) {
+        for (var i=0; i<latest.bed_exit_periods.length; i++) {
+          latest.bed_exit_periods[i] = {
+            exit_start: new Date(latest.bed_exit_periods[i][0] * 1000),
+            exit_end: new Date(latest.bed_exit_periods[i][1] * 1000),
+            exit_duration: latest.bed_exit_periods[i][1] - latest.bed_exit_periods[i][0]
+          }
+        }
+      }
+      return c8.type(presenceIndex).insert(latest).then(function(result) {
+        console.log(latest.timestamp);
       }).catch(function(error) {
         console.trace(error);
       });
+*/
     })
-    qs.trends(deviceId).then(function(trends) {
+      qs.trends(deviceId).then(function(trends) {
+      if (trends.error) {
+        console.error('Could not index trends: ' + trends.error);
+        return;
+      }
       var bulk = [];
       for (var i=0; i<trends.length; i++) {
         var id = trends[i].date;
