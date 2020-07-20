2a0b8e28b30a861adf517857ca8a7e723fd99c04
Samuel Rinnetmäki <samuel@iki.fi>
2020-07-20 09:02:47 +0300

    using Elastic Common Schema

--- expense/index.js
+++ expense/index.js
@@ -1,5 +1,9 @@
 var request = require("request");
 
+// if no start date is given as an argument,
+// fetch data from last DEFAULT_DAYS days
+var DEFAULT_DAYS = 30;
+
 var adapter = {};
 
 adapter.sensorName = 'expense';
@@ -8,12 +12,27 @@ adapter.types = [
   {
     name: 'expense',
     fields: {
-      date: 'date',
-      c: 'integer',
-      category: 'string',
-      t: 'float',
-      type: 'string',
-      cost: 'float'
+      "@timestamp": 'date',
+      "ecs": {
+        "version": 'keyword'
+      },
+      "event": {
+        "created": "date",
+        "module": "keyword",
+        "original": "keyword",
+        "start": "date",
+      },
+      "expense": {
+        "category": {
+          "id": 'long',
+          "name": 'keyword'
+        },
+        "type": {
+          "id": 'float',
+          "name": 'keyword'
+        },
+        "cost": 'float'
+      }
     }
   }
 ];
@@ -36,84 +55,88 @@ adapter.storeConfig = function(c8, result) {
 adapter.importData = function(c8, conf, opts) {
   return new Promise(function (fulfill, reject){
     var firstDate, lastDate;
-    return c8.search({
-      _source: ['timestamp'],
-      size: 1,
-      sort: [{'timestamp': 'desc'}],
-    }).then(function(response) {
-      var resp = c8.trimResults(response);
-      var firstDate, lastDate;
-      if (opts.firstDate) {
-        firstDate = new Date(opts.firstDate);
-        console.log('Setting first time to ' + firstDate);
+    if (opts.firstDate) {
+      firstDate = new Date(opts.firstDate);
+      console.log('Setting first time to ' + firstDate);
+    }
+    else {
+      var d = new Date();
+      firstDate = new Date(d.getTime() - DEFAULT_DAYS * 24 * 60 * 60 * 1000);
+      console.log('Setting first time to ' + firstDate);
+    }
+    var url = conf.url + '&from=' + firstDate.getDate() + '.' + (firstDate.getMonth() + 1) + '.' + firstDate.getFullYear();
+    if (opts.lastDate) {
+      var lastDate = opts.lastDate;
+      url += '&to=' + lastDate.getDate() + '.' + (lastDate.getMonth() + 1) + '.' + lastDate.getFullYear();
+    }
+    var cookieJar = request.jar();
+    return request({url: url, jar: cookieJar}, function(error, response, body) {
+      if (error || !response || !body) {
+        reject(new Error('Error getting data: ' + JSON.stringify(response.body)));
       }
-      else if (resp && resp.timestamp) {
-        var d = new Date(resp.timestamp);
-        firstDate = new Date(d.getTime() + 1);
-        console.log('Setting first time to ' + firstDate);
-      }
-      else {
-        console.warn("No previously indexed data, setting first time to 0!");
-        firstDate = new Date(0);
-      }
-      var url = conf.url + '&from=' + firstDate.getDate() + '.' + (firstDate.getMonth() + 1) + '.' + firstDate.getFullYear();
-      if (opts.lastDate) {
-        var lastDate = opts.lastDate;
-        url += '&to=' + lastDate.getDate() + '.' + (lastDate.getMonth() + 1) + '.' + lastDate.getFullYear();
-      }
-      var cookieJar = request.jar();
-      return request({url: url, jar: cookieJar}, function(error, response, body) {
-        if (error || !response || !body) {
-          reject(new Error('Error getting data: ' + JSON.stringify(response.body)));
-        }
-        var data = JSON.parse(body);
-        if (data && data.length) {
-          var bulk = [];
-          for (var i=0; i<data.length; i++) {
-            var dayData = data[i];
-            var dayCost = 0;
-            for (var j=0; j<dayData.length; j++) {
-              var id = dayData[j].date + '-' + dayData[j].t;
-              bulk.push({index: {_index: c8._index, _type: c8._type, _id: id}});
-              dayData[j].id = id;
-              dayData[j].timestamp = dayData[j].date;
-              bulk.push(dayData[j]);
-              dayCost += dayData[j].cost;
-            }
-            console.log(dayData[0].date + ': ' + dayCost);
-          }
-          if (bulk.length > 0) {
-            c8.bulk(bulk).then(function(result) {
-              if (result.errors) {
-                var messages = [];
-                for (var i=0; i<result.items.length; i++) {
-                  if (result.items[i].index.error) {
-                    messages.push(i + ': ' + result.items[i].index.error.reason);
-                  }
-                }
-                reject(new Error(messages.length + ' errors in bulk insert:\n ' + messages.join('\n ')));
-                c8.release();
-                return;
+      var dates = JSON.parse(body);
+      if (dates && dates.length) {
+        var bulk = [];
+        for (var i=0; i<dates.length; i++) {
+          var dayData = dates[i];
+          var dayCost = 0;
+          for (var j=0; j<dayData.length; j++) {
+            var id = dayData[j].date + '-' + dayData[j].t;
+            let data = {
+              "@timestamp": dayData[j].date,
+              "event": {
+                "created": new Date(),
+                "module": "expense",
+                "original": JSON.stringify(dayData[j]),
+                "start": dayData[j].date,
+              },
+              "expense": {
+                "category": {
+                  "id": dayData[j].c,
+                  "name": dayData[j].category,
+                },
+                "type": {
+                  "id": dayData[j].t,
+                  "name": dayData[j].type,
+                },
+                "cost": dayData[j].cost
               }
-              fulfill('Indexed ' + result.items.length + ' documents in ' + result.took + ' ms.');
-              c8.release();
-            }).catch(function(error) {
-              reject(error);
-              c8.release();
-            });
+            };
+            bulk.push({index: {_index: c8._index, _type: c8._type, _id: id}});
+            bulk.push(data);
+            dayCost += dayData[j].cost;
           }
-          else {
-            fulfill('No data available');
+          console.log(dayData[0].date + ': ' + dayCost);
+        }
+        if (bulk.length > 0) {
+          c8.bulk(bulk).then(function(res) {
+            let result = c8.trimResults(res);
+            if (result.errors) {
+              var messages = [];
+              for (var i=0; i<result.items.length; i++) {
+                if (result.items[i].index.error) {
+                  messages.push(i + ': ' + result.items[i].index.error.reason);
+                }
+              }
+              reject(new Error(messages.length + ' errors in bulk insert:\n ' + messages.join('\n ')));
+              c8.release();
+              return;
+            }
+            fulfill('Indexed ' + result.items.length + ' documents in ' + result.took + ' ms.');
             c8.release();
-          }
+          }).catch(function(error) {
+            reject(error);
+            c8.release();
+          });
         }
         else {
           fulfill('No data available');
+          c8.release();
         }
-      });
-    }).catch(function(error) {
-      reject(error);
-      c8.release();
+      }
+      else {
+        fulfill('No data available');
+      }
     });
   });
 }
