d5c580cb849b14ded10d32cb9f343ac92f5ae8df
Samuel Rinnetmäki <samuel@iki.fi>
2020-09-21 07:36:31 +0300

    using moment and tar, ECS changes

--- googlelocation/index.js
+++ googlelocation/index.js
@@ -1,12 +1,11 @@
 const JSONStream = require('JSONStream');
 const {google} = require('googleapis');
-const compressing = require('compressing');
-const unzip = require('unzip-stream');
+const tar = require('tar');
 const eos = require('end-of-stream');
 const prompt = require('prompt');
 const fs = require('fs');
 const request = require('request');
-
+const moment = require('moment');
 
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
 
@@ -17,7 +16,7 @@ const MAX_BULK_BATCH = 10000;
 
 var adapter = {};
 let finishedBatches = 0;
-let stream;
+let driveStream;
 
 adapter.sensorName = 'googlelocation';
 
@@ -25,18 +24,51 @@ adapter.types = [
   {
     name: adapter.sensorName,
     fields: {
-      timestamp: 'date',
-      timestampMs: 'keyword',
-      latitudeE7: 'long',
-      longitudeE7: 'long',
-      coords: 'geo_point',
-      accuracy: 'integer',
-      altitude: 'integer',
-      activity: {
-        timestampMs: 'keyword',
-        activity: {
-          type: 'keyword',
-          confidence: 'integer',
+      "@timestamp": "date",
+      "ecs": {
+        "version": 'keyword'
+      },
+      "event": {
+        "created": "date",
+        "dataset": "keyword",
+        "module": "keyword",
+        "original": "keyword",
+        "start": "date",
+        "timezone": "keyword"
+      },
+      "date_details": {
+        "year": 'long',
+        "month": {
+          "number": 'long',
+          "name": 'keyword',
+        },
+        "week_number": 'long',
+        "day_of_year": 'long',
+        "day_of_month": 'long',
+        "day_of_week": {
+          "number": 'long',
+          "name": 'keyword',
+        }
+      },
+      "time_slice": {
+        "start_hour": 'long',
+        "id": 'long',
+        "name": 'keyword',
+      },
+      "geo": {
+        "location": "geo_point"
+      },
+      "location": {
+        "latitudeE7": 'long',
+        "longitudeE7": 'long',
+        "accuracy": 'integer',
+        "altitude": 'integer',
+      },
+      "activity": {
+        "timestampMs": 'keyword',
+        "activity": {
+          "type": 'keyword',
+          "confidence": 'integer',
         }
       }
     }
@@ -155,7 +187,7 @@ adapter.importData = function(c8, conf, opts) {
           console.log('Processing file ' + i + ': ' + fileName);
           
 /*
-          stream = drive.files.get({
+          driveStream = drive.files.get({
             auth: oauth2Client,
             fileId: file.id,
             alt: 'media'
@@ -168,72 +200,81 @@ adapter.importData = function(c8, conf, opts) {
             token: conf.credentials.access_token
           }
           let url = 'https://www.googleapis.com/drive/v3/files/' + file.id + '?alt=media';
-          stream = request.get({url: url, headers: {'Authorization': 'Bearer ' + oauth2Client.credentials.access_token}});
-          if (!stream) {
+          driveStream = request.get({url: url, headers: {'Authorization': 'Bearer ' + oauth2Client.credentials.access_token}});
+          if (!driveStream) {
             console.error('stream failed for file ' + file.id + '!');
             continue;
           }
 /*
-          eos(stream, function(err) {
-            console.log('End of main stream!');
+          eos(driveStream, function(err) {
+            console.log('End of main driveStream!');
             if (err) {
               return console.log('stream had an error or closed early');
             }
           });
 */
-          stream
-          .setMaxListeners(MAX_ZIP_ENTRIES)
+          driveStream
+          // .setMaxListeners(MAX_ZIP_ENTRIES)
           .on('error', function (error) {
-            console.error(new Error('Stream error: ' + error));
+            console.error(new Error('driveStream error: ' + error));
             return;
           })
-          .pipe(new compressing.tgz.UncompressStream())
-          .on('error', function (error) {
-            console.error(new Error('JSONstream error: ' + error));
-            return;
-          })
-          .on('entry', function(header, substream, next) {
-            if (header.type != 'file' || header.name.indexOf('.json') < 0) {
-              console.log('Skipping ' + header.name);
-              return;
+          .pipe(tar.x({filter: (path, entry) => {
+            if (path.indexOf('.json') > 0) {
+              // console.log('Processing ' + path);
+              return true;
             }
-            console.log(header.mtime + ': ' + header.name + ' (' + Math.round(header.size/1024) + ' kB)');
+            console.log('Skipping ' + path);
+            return false;
+          }}))
+          .on('error', function (error) {
+            console.error(new Error('tar.x stream error: ' + error));
+            return;
+          })
+          .on('entry', function(substream) {
+            // console.log(substream);
+            console.log(substream.mtime + ': ' + substream.path + ' (' + Math.round(substream.size/1024) + ' kB)');
             eos(substream, function(err) {
               if (err) {
-                return console.log('stream had an error or closed early');
-              }
-              // console.log('stream has ended', this === substream);
-              // The main stream (read files from Google Drive) never ends!
-              // Luckily, the zip files only contain one entry. We can consider
-              // the whole file processed when the first zip archive entry ends.
-              if (finishedBatches > 0) {
-                var updateParams = {
-                  auth: oauth2Client,
-                  fileId: file.id,
-                  addParents: conf.outputDir,
-                  removeParents: conf.inputDir,
-                  fields: 'id, parents'
-                };
-                drive.files.update(updateParams, function(err, updated) {
-                  if(err) {
-                    reject(new Error(err));
-                    return;
-                  }
-                  else {
-                    fulfill('Moved ' + file.name + ' from ' + conf.inputDir + ' to ' + conf.outputDir);
-                  }
-                });
-              }
-              else {
-                console.log('No location history in ' + file.name);
+                return console.log('tar entry stream had an error or closed early');
               }
             });
             let parse = JSONStream.parse('locations.*');
             substream.pipe(parse)
             .on('data', function(data) {
               // console.log(JSON.stringify(data));
-              data.timestamp = new Date(Number(data.timestampMs));
-              data.coords = data.latitudeE7/10E6 + ',' + data.longitudeE7/10E6;
+              let start = moment(Number(data.timestampMs));
+              let values = {
+                "@timestamp": start.format(),
+                "ecs": {
+                  "version": "1.0.1"
+                },
+                "event": {
+                  "created": new Date(),
+                  "dataset": "google.location",
+                  "module": "Takeout",
+                  "original": JSON.stringify(data),
+                  "start":  start.format(),
+                },
+                "time_slice": time2slice(start),
+                "date_details": {
+                  "year": start.format('YYYY'),
+                  "month": {
+                    "number": start.format('M'),
+                    "name": start.format('MMMM'),
+                  },
+                  "week_number": start.format('W'),
+                  "day_of_year": start.format('DDD'),
+                  "day_of_month": start.format('D'),
+                  "day_of_week": {
+                    "number": start.format('d'),
+                    "name": start.format('dddd'),
+                  }
+                },
+                "geo": {},
+                "activity": data
+              };
+              values.geo.location = data.latitudeE7/10E6 + ',' + data.longitudeE7/10E6;
               // console.log(JSON.stringify(data));
               let meta = {
                 index: {
@@ -243,18 +284,18 @@ adapter.importData = function(c8, conf, opts) {
               bulk.push(meta);
               bulk.push(data);
               if (bulk.length >= (MAX_BULK_BATCH * 2)) {
-                stream.pause();
+                driveStream.pause();
                 // console.log(JSON.stringify(bulk, null, 1));
                 // return;
                 let clone = bulk.slice(0);
                 bulk = [];
                 results.push(indexBulk(clone, conf, c8).catch(reject));
                 console.log('Started ' + results.length + ' bulk batches (' + clone[1].timestamp + ')');
-                // setTimeout(stream.resume, BULK_BATCH_MS);
+                // setTimeout(driveStream.resume, BULK_BATCH_MS);
               }
             })
             .on('end', function() {
-              console.log('Last batch of ' + header.name + '!');
+              console.log('Last batch of ' + substream.path + '!');
               if (bulk.length > 0) {
                 results.push(indexBulk(bulk, conf, c8).catch(reject));
               }
@@ -262,13 +303,37 @@ adapter.importData = function(c8, conf, opts) {
               // next();
             })
             .on('error', (error) => {
-              console.log('Error processing ' + header.name);
+              console.log('Error processing ' + substream.path);
               console.log(new Error(error));
               // reject(error);
             });
           })
-          .on('end', function() {
+          .on('finished', function() {
             console.log('Happy ending!');
+              if (finishedBatches > 0) {
+                var updateParams = {
+                  auth: oauth2Client,
+                  fileId: file.id,
+                  addParents: conf.outputDir,
+                  removeParents: conf.inputDir,
+                  fields: 'id, parents'
+                };
+ /*
+               drive.files.update(updateParams, function(err, updated) {
+                  if(err) {
+                    reject(new Error(err));
+                    return;
+                  }
+                  else {
+                    fulfill('Moved ' + file.name + ' from ' + conf.inputDir + ' to ' + conf.outputDir);
+                  }
+                });
+ */
+                fulfill('(fake) Moved ' + file.name + ' from ' + conf.inputDir + ' to ' + conf.outputDir);
+              }
+              else {
+                fulfill('No location history in ' + file.name);
+              }
           })
           .on('error', err => {reject(new Error(err))});
         }
@@ -297,11 +362,28 @@ function indexBulk(bulkData, oonf, c8) {
         }
       }
       console.log('Finished ' + (++finishedBatches) + ' bulk batches.');
-      stream.resume();
+      driveStream.resume();
       // process.stdout.write('>');
       fulfill(result);
     }).catch(reject);
   });
+}
+
+function time2slice(t) {
+  // creates a time_slice from a moment object
+  let time_slice = {};
+  let hour = t.format('H');
+  let minute = (5 * Math.floor(t.format('m') / 5 )) % 60;
+  time_slice.name = [hour, minute].join(':');
+  if (minute == 5) {
+    time_slice.name = [hour, '0' + minute].join(':');
+  }
+  else if (minute == 0) {
+    time_slice.name += '0';
+  }
+  let idTime = parseInt(hour) + parseInt(minute)/60;
+  time_slice.id = Math.round((idTime + (idTime >= 4 ? -4 : 20)) * 12);
+  return time_slice;
 }
 
 module.exports = adapter;
