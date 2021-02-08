1ef66a4353bcada0137c17919a2a7287f68802d2
Samuel Rinnetmäki <samuel@iki.fi>
2021-02-08 12:14:34 +0200

    Using newer version of Nordpool module

--- nordpool/index.js
+++ nordpool/index.js
@@ -1,22 +1,64 @@
-var nordpool = require("nordpool");
-var prices = new nordpool.Prices()
+const nordpool = require("nordpool")
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
 
-var MS_IN_DAY = 24 * 60 * 60 * 1000;
+const prices = new nordpool.Prices()
 
-var adapter = {};
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
-adapter.sensorName = 'nordpool-price-hourly';
+const MS_IN_DAY = 24 * 60 * 60 * 1000
+
+const adapter = {}
+
+adapter.sensorName = 'nordpool-price-hourly'
 
 adapter.types = [
   {
     name: 'nordpool-price-hourly',
-    fields: {
-      timestamp: 'date',
-      area: 'string',
-      value: 'float'
+    "fields": {
+      "@timestamp": "date",
+      "ecs": {
+        "version": 'keyword'
+      },
+      "event": {
+        "created": "date",
+        "dataset": "keyword",
+        "duration": "long",
+        "end": "date",
+        "module": "keyword",
+        "original": "keyword",
+        "start": "date",
+        // "timezone": "keyword"
+      },
+      "date_details": {
+        "year": 'long',
+        "month": {
+          "number": 'long',
+          "name": 'keyword',
+        },
+        "week_number": 'long',
+        "day_of_year": 'long',
+        "day_of_month": 'long',
+        "day_of_week": {
+          "number": 'long',
+          "name": 'keyword',
+        }
+      },
+      "time_slice": {
+        "start_hour": 'long',
+        "id": 'long',
+        "name": 'keyword',
+      },
+      "nordpool": {
+        "area": 'string',
+        "value": 'float',
+        "price": 'float'
+      }
     }
   }
-];
+]
 
 adapter.promptProps = {
   properties: {
@@ -29,99 +71,94 @@ adapter.promptProps = {
       default: 'EUR'
     }
   }
-};
-
-adapter.storeConfig = function(c8, result) {
-  return c8.config(result).then(function(){
-    console.log('Configuration stored.');
-    c8.release();
-  });
 }
 
-adapter.importData = function(c8, conf, opts) {
-  return new Promise(function (fulfill, reject){
-    var firstDate, lastDate;
-    c8.search({
-      _source: ['timestamp'],
-      size: 1,
-      sort: [{'timestamp': 'desc'}],
-    }).then(function(response) {
-      var resp = c8.trimResults(response);
-      console.log('Getting last date...');
-      if (opts.firstDate) {
-        firstDate = opts.firstDate;
-        if (typeof(firstDate) != 'Date') {
-          firstDate = new Date(firstDate);
-          console.log("Setting firstDate to " + firstDate.toISOString());
+adapter.storeConfig = async (c8, result) => {
+  await c8.config(result)
+  console.log('Configuration stored.')
+}
+
+adapter.importData = async (c8, conf, opts) => {
+  try {
+    let firstDate, lastDate
+    if (opts.firstDate) {
+      firstDate = dayjs(opts.firstDate)
+      console.log('Setting first time to ' + firstDate.format())
+    }
+    else if (!opts.lastDate) {
+      const response = await c8.search({
+        _source: ['@timestamp'],
+        size: 1,
+        sort: [{'@timestamp': 'desc'}],
+      })
+      const resp = c8.trimResults(response)
+      if (resp && resp["@timestamp"]) {
+        firstDate = dayjs(resp["@timestamp"])
+      }
+    }
+    if (opts.lastDate) {
+      lastDate = dayjs(opts.lastDate)
+      // console.log("Setting lastDate to " + lastDate.format())
+      if (!firstDate) {
+        firstDate = lastDate.subtract(1, 'day')
+        console.log("Setting firstDate to " + firstDate.format())
+      }
+    }
+    else if (firstDate) {
+      // lastDate = firstDate.add(1, 'day')
+    }
+    else {
+      firstDate = dayjs()
+      // lastDate = firstDate.add(1, 'day')
+    }
+    // don't specify "to" param, it'll just strip out stuff we want to index
+    var params = {area: conf.area, currency: conf.currency, from: firstDate.format()}
+    // console.log(params)
+    if (firstDate) {
+      params.from = firstDate
+    }
+    const data = await prices.hourly(params)
+    if (data && data.length) {
+      var bulk = []
+      for (row of data) {
+        const st = dayjs(row.date).tz("Europe/Oslo")
+        const et = st.add(1, 'hour')
+        const id = 'price-hourly-' + row.area + '-' + st.format()
+        const values = c8.initECS("nordpool", row, st.toISOString(), et.toISOString(), null, 'metric', 'nordpool.prices')
+        values.nordpool = row
+        // EUR / MWh => snt/kWh, add VAT 24 %
+        values.nordpool.price = Math.round(values.nordpool.value * 1.24 * 100) / 1000
+        bulk.push({index: {_index: c8._index, _id: id}})
+        bulk.push(values)
+        console.log(id + ': ' + values.nordpool.price)
+      }
+      // console.log(JSON.stringify(bulk, null, 1))
+      if (bulk.length > 0) {
+        const res = await c8.bulk(bulk)
+        let result = c8.trimResults(res)
+        if (result.errors) {
+          var messages = []
+          for (var i=0; i<result.items.length; i++) {
+            if (result.items[i].index.error) {
+              messages.push(i + ': ' + result.items[i].index.error.reason)
+            }
+          }
+          throw new Error(messages.length + ' errors in bulk insert:\n ' + messages.join('\n '))
+          return
         }
-      }
-      else if (opts.lastDate) {
-        lastDate = opts.lastDate;
-        if (typeof(lastDate) != 'Date') {
-          lastDate = new Date(lastDate);
-          console.log("Setting lastDate to " + lastDate.toISOString());
+        return 'Indexed ' + result.items.length + ' documents in ' + result.took + ' ms.'
         }
-      }
-      else if (resp && resp.timestamp) {
-          firstDate = new Date(resp.timestamp);
-        console.log("Setting firstDate to last found " + firstDate.toISOString());
-      }
       else {
-        console.warn("No previously indexed data, setting lastDate to today!");
-        lastDate = new Date();
+        throw new Error('Got data but could not parse indexable items!')
       }
-      var params = {area: conf.area, currency: conf.currency, to: lastDate};
-      if (firstDate) {
-        params.from = firstDate;
-      }
-      // console.log(params);
-      prices.hourly(params, function(error, data) {
-        if (error) {
-          reject(error);
-          return;
-        }
-        // console.log(JSON.stringify(data, null, 2));
-        if (data && data.length) {
-          var bulk = [];
-          for (var i=0; i<data.length; i++) {
-            var row = data[i];
-            var ts = row.date.format();
-            var id = 'price-hourly-' + ts + '-' + row.area;
-            var values = {area: row.area, value: row.value, timestamp: ts};
-            bulk.push({index: {_index: c8._index, _type: c8._type, _id: id}});
-            bulk.push(values);
-            console.log(id + ': ' + row.value);
-          }
-          if (bulk.length > 0) {
-            c8.bulk(bulk).then(function(response) {
-              let result = c8.trimBulkResults(response);
-              if (result.errors) {
-                var messages = [];
-                for (var i=0; i<result.items.length; i++) {
-                  if (result.items[i].index.error) {
-                    messages.push(i + ': ' + result.items[i].index.error.reason);
-                  }
-                }
-                reject(new Error(messages.length + ' errors in bulk insert:\n ' + messages.join('\n ')));
-              }
-              fulfill('Indexed ' + result.items.length + ' documents in ' + result.took + ' ms.');
-            }).catch(function(error) {
-              reject(error);
-              bulk = null;
-            });
-          }
-          else {
-            fulfill('No data available');
-          }
-        }
-        else {
-          fulfill('No data available');
-          // fulfill(JSON.stringify(data.Rows, null, 2));
-        }
-      });
-    }).catch(function(error) {
-      reject(error);
-    });
-  });
+    }
+    else {
+      return 'No data available'
+    }
+  }
+  catch(e) {
+    throw new Error(e)
+  }
+  return "(in async mode...)"
 }
-module.exports = adapter;
+module.exports = adapter
